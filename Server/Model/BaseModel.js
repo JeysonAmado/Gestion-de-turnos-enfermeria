58b937dd8450de49752d2d5a429808ac2e476dd6
JeysonAmado <jeyson.amado@savne.net>
2023-01-11 17:30:41 -0500

    Se creó esquemas de la base de datos

--- Server/Model/BaseModel.js
+++ Server/Model/BaseModel.js
@@ -1,7 +1,32 @@
 const {Bed, BedSchema } = require('./BedsModel');
+const { NurseArea, NurseAreaSchema } = require('./NurseArea');
+const { NurseCharge, NurseChargeSchema } = require('./NurseChargeModel');
+const { Nurse, NurseSchema } = require('./NurseModel');
+const { NurseShift, NurseShiftSchema } = require('./NurseShift');
+const { Pacient, PacientSchema } = require('./PacientModel');
+const { SeverityLevel, SeverityLevelSchema } = require('./SeverityLevelModel');
 
 function setupModels(sequelize){
+    SeverityLevel.init(SeverityLevelSchema,SeverityLevel.config(sequelize));
     Bed.init(BedSchema, Bed.config(sequelize));
+    //NurseArea.init(NurseAreaSchema,NurseArea.config(sequelize));
+    //NurseCharge.init(NurseChargeSchema,NurseCharge.config(sequelize));
+    //NurseShift.init(NurseShiftSchema,NurseShift.config(sequelize));
+    //Nurse.init(NurseSchema,Nurse.config(sequelize));
+    Pacient.init(PacientSchema,Pacient.config(sequelize));
+
+   associateModels(sequelize);
+}
+
+function associateModels(sequelize){
+    SeverityLevel.associate(sequelize.models);
+    Bed.associate(sequelize.models);
+    //NurseArea.associate(sequelize.models);
+    //NurseCharge.associate(sequelize.models);
+    //NurseShift.associate(sequelize.models);
+    Nurse.associate(sequelize.models);
+    Pacient.associate(sequelize.models);
+
 }
 
 
