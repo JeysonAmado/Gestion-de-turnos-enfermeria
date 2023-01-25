e1b7aa635713550eb393977fb22cd825171d9947
JeysonAmado <jeyson.amado@savne.net>
2023-01-25 10:54:48 -0500

    Procedimientos agregados en setupModels

--- Server/Model/BaseModel.js
+++ Server/Model/BaseModel.js
@@ -5,6 +5,18 @@ const { Nurse, NurseSchema } = require('./Nurses/NurseModel');
 const { NurseShift, NurseShiftSchema } = require('./Nurses/NurseShiftModel');
 const { Pacient, PacientSchema } = require('./Pacients/PacientModel');
 const { SeverityLevel, SeverityLevelSchema } = require('./Pacients/SeverityLevelModel');
+const { Bath, BathSchema } = require('./Procediments/BathModel');
+const { BathType, BathTypeSchema } = require('./Procediments/ProcedimentsTypes/BathTypesModel');
+const { Canalization, CanalizationSchema } = require('./Procediments/CanalizationModel');
+const { DiagnosticImage, DiagnosticImagesSchema } = require('./Procediments/DiagnosticImageModel');
+const { DiagnosticImageType, DiagnosticImagesTypeSchema } = require('./Procediments/ProcedimentsTypes/DiagnosticImagesTypesModel');
+const { Laboratory, LaboratorySchema } = require('./Procediments/LaboratoryModel');
+const { LaboratoryType, LaboratoryTypeSchema } = require('./Procediments/ProcedimentsTypes/LaboratoryTypeModel');
+const { MedicineSupplied, MedicineSuppliedSchema } = require('./Procediments/MedicineSuppliedModel');
+const { Probe, ProbeSchema } = require('./Procediments/ProbeModel');
+const { ProbeType, ProbeTypeSchema } = require('./Procediments/ProcedimentsTypes/ProbesTypeModel');
+const { ShiftCheckUp, ShiftCheckUpSchema } = require('./Procediments/ShiftsCheckUpModel');
+const { Signals, SignalsSchema } = require('./Procediments/SignalsModel');
 
 function setupModels(sequelize){
     SeverityLevel.init(SeverityLevelSchema,SeverityLevel.config(sequelize));
@@ -15,7 +27,20 @@ function setupModels(sequelize){
     Nurse.init(NurseSchema,Nurse.config(sequelize));
     Pacient.init(PacientSchema,Pacient.config(sequelize));
 
-    //associateModels(sequelize);
+    //Procediments
+    Bath.init(BathSchema,Bath.config(sequelize));
+    BathType.init(BathTypeSchema,BathType.config(sequelize));
+    Canalization.init(CanalizationSchema,Canalization.config(sequelize));
+    DiagnosticImage.init(DiagnosticImagesSchema,DiagnosticImage.config(sequelize));
+    DiagnosticImageType.init(DiagnosticImagesTypeSchema,DiagnosticImageType.config(sequelize));
+    Laboratory.init(LaboratorySchema, Laboratory.config(sequelize));
+    LaboratoryType.init(LaboratoryTypeSchema,LaboratoryType.config(sequelize));
+    MedicineSupplied.init(MedicineSuppliedSchema,MedicineSupplied.config(sequelize));
+    Probe.init(ProbeSchema,Probe.config(sequelize));
+    ProbeType.init(ProbeTypeSchema,ProbeType.config(sequelize));
+    ShiftCheckUp.init(ShiftCheckUpSchema,ShiftCheckUp.config(sequelize));
+    Signals.init(SignalsSchema,Signals.config(sequelize));
+
 }
 
 function associateModels(sequelize){
